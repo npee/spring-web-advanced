650fe380b50e99bb67638ae665d4b6982fc47e55
npee <jmc4664@gmail.com>
2021-10-24 14:45:51 +0900

    Initial Commit

---
+++ src/main/java/io/npee/springwebadvanced/SpringWebAdvancedApplication.java
@@ -0,0 +1,13 @@
+package io.npee.springwebadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebAdvancedApplication.class, args);
+    }
+
+}
