a9ae22802b52f53827337a6f75c837899b5dcdc6
npee <jmc4664@gmail.com>
2021-10-25 22:49:10 +0900

    로그 추적기 v0 - 패키지 이동

--- src/main/java/io/npee/springwebadvanced/app/v0/OrderControllerV0.java
+++ src/main/java/io/npee/springwebadvanced/app/v0/OrderControllerV0.java
@@ -1,4 +1,4 @@
-package io.npee.springwebadvanced.logtracker.app.v0;
+package io.npee.springwebadvanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
