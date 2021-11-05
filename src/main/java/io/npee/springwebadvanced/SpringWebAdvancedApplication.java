ad0a0bb19c0ab30f11b98c806f6321c9df1a35e7
npee <jmc4664@gmail.com>
2021-11-06 03:13:51 +0900

    특정 구성 파일 임포트 및 특정 패키지 하위 파일들만 컴포넌트 스캔 대상으로 설정

--- src/main/java/io/npee/springwebadvanced/SpringWebAdvancedApplication.java
+++ src/main/java/io/npee/springwebadvanced/SpringWebAdvancedApplication.java
@@ -1,9 +1,17 @@
 package io.npee.springwebadvanced;
 
+import io.npee.springwebadvanced.app.v6.AppV6Config;
+import io.npee.springwebadvanced.app.v7.AppV7Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import({AppV6Config.class, AppV7Config.class})
+@SpringBootApplication(scanBasePackages = {
+        "io.npee.springwebadvanced.app.v6",
+        "io.npee.springwebadvanced.app.v7",
+        "io.npee.springwebadvanced.app.v8"
+})
 public class SpringWebAdvancedApplication {
 
     public static void main(String[] args) {
