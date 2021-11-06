284e2a1c797e2c8f10572677e9705aa8284b25c0
npee <jmc4664@gmail.com>
2021-11-06 16:58:09 +0900

    데코레이터 패턴 - MessageDecorator 적용

--- src/test/java/io/npee/springwebadvanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/io/npee/springwebadvanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package io.npee.springwebadvanced.proxy.pureproxy.decorator;
 
 import io.npee.springwebadvanced.proxy.pureproxy.decorator.code.Component;
 import io.npee.springwebadvanced.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import io.npee.springwebadvanced.proxy.pureproxy.decorator.code.MessageDecorator;
 import io.npee.springwebadvanced.proxy.pureproxy.decorator.code.RealComponent;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,14 @@ public class DecoratorPatternTest {
     void noDecorator() {
         Component component = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(component);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component component = new RealComponent();
+        Component decorator = new MessageDecorator(component);
+        DecoratorPatternClient client = new DecoratorPatternClient(decorator);
         client.execute();
     }
 }
