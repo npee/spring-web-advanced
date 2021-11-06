cd25b884a442f6f0de88b684be9d1ba7662c0609
npee <jmc4664@gmail.com>
2021-11-06 16:35:15 +0900

    프록시 패턴 - 프록시 패턴 적용

--- src/test/java/io/npee/springwebadvanced/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/io/npee/springwebadvanced/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package io.npee.springwebadvanced.proxy.pureproxy.proxy;
 
+import io.npee.springwebadvanced.proxy.pureproxy.proxy.code.CacheProxy;
 import io.npee.springwebadvanced.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import io.npee.springwebadvanced.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject subject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(subject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject subject = new RealSubject();
+        CacheProxy proxy = new CacheProxy(subject);
+        ProxyPatternClient client = new ProxyPatternClient(proxy);
         client.execute();
         client.execute();
         client.execute();
